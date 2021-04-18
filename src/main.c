4790754f48269d407c0c99be7c9d6c9c7f0e462d
ari <aristide.ff@gmail.com>
2021-04-18 21:59:46 +0200

    fini
    oublie pas de créer un fichier répertoire entrepôt a la racine du projet

--- src/main.c
+++ src/main.c
@@ -1,82 +1,100 @@
+#include <stdio.h>
+#include <string.h>
 #include <curl/curl.h>
- #define MAX_SIZE 80
-/* <DESC>
- * Get a single file from an FTP server.
- * </DESC>
- */ 
- 
-struct FtpFile {
-  const char *filename;
-  FILE *stream;
-};
- 
-static size_t my_fwrite(void *buffer, size_t size, size_t nmemb, void *stream)
-{
-  struct FtpFile *out = (struct FtpFile *)stream;
-  if(!out->stream) {
-    /* open file for writing */ 
-    out->stream = fopen(out->filename, "wb");
-    if(!out->stream)
-      return -1; /* failure, can't open file to write */ 
+#include "structCurl.h" 
+#include "funcCurl.c"
+
+#define IPTAILLE   32
+
+char * nameFile(char *url, char * file, int i){
+  time_t timestamp = time( NULL );
+  struct tm * pTime = localtime( & timestamp );
+  char day[ MAX_SIZE ];
+  char mmaa[ MAX_SIZE ];
+  switch(i){
+    case 0:
+      sprintf(url,"ftp://ari:ari@192.168.1.25/Documents/entrepot/");
+      break;
+    case 1:
+      sprintf(url,"ftp://ari:ari@192.168.1.21/Documents/entrepot/");
+      break;  
+
   }
-  return fwrite(buffer, size, nmemb, out->stream);
+  strftime( day, MAX_SIZE, "%d", pTime );
+  strftime( mmaa, MAX_SIZE, "%m%y", pTime );
+  strcat(file,day);
+  switch(i){
+    case 0:
+      strcat( file,"Paris");
+      break;
+    case 1:
+      strcat(file,"Nante");  
+  }
+  strcat( file,mmaa);
+  strcat( file,".xml");  
+  strcat(url, file);
+  return url;
+
 }
- 
+
+char * nameWarahouse(char * entrepot, char * filename){
+  FILE* fichier = NULL;
+  time_t timestamp = time( NULL );
+  struct tm * pTime = localtime( & timestamp );
+  char mmaa[ MAX_SIZE ];
+  strftime( mmaa, MAX_SIZE, "%m%y", pTime );
+  strcpy(entrepot,"entrepot/");
+  strcat( entrepot,mmaa);
+  mkdir(entrepot);
+  strcat(filename, entrepot);
+  strcat(filename,"/entrepot.xml");
+  fichier =  fopen(filename, "w+");
+  fputs("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n", fichier);
+  fputs("<entrepots>",fichier);
+  fclose(fichier);
+  return filename;
+}
+
+int fusion(){
+  char *strXML ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n";
+  FILE * fp = NULL;
+  FILE *fp1;
+  char ch;
+  char *entrepot = malloc(sizeof(char)*20);
+  char *filename = malloc(sizeof(char)*27);
+  char * repertory = nameWarahouse(entrepot,filename);
+  fp =  fopen(repertory,"a");
+  char *url = malloc(sizeof(char)*120);
+  char *file = malloc(sizeof(char)*70);
+  for (int i = 0; i <2; i++) {
+    char buf[255];
+    strcpy(url, "");
+    strcpy(file, "");
+    nameFile(url, file, i);
+    download(url, file);
+    fp1 = fopen(file, "r");
+    int y =0;
+    while(fgets(buf, sizeof buf, fp1) != NULL){
+      if (y==0){
+        y++;
+      }else{
+        fprintf(fp,"\t%s",buf);
+      }
+    }    
+    fclose(fp1);
+    remove(file);  
+  }
+  fprintf(fp,"</entrepots>");
+  fclose(fp);
+  free(url);
+  free(file);
+  return 1;
+}
  
 int main(void)
 {
-  time_t timestamp = time( NULL );
-  struct tm * pTime = localtime( & timestamp );
-  char repertory[120]="ftp://ari:ari@192.168.1.25/Documents/entrepot/";
-  char file[70];
-  char day[ MAX_SIZE ];
-  char mmaa[ MAX_SIZE ];
-  strftime( day, MAX_SIZE, "%d", pTime );
-  strftime( mmaa, MAX_SIZE, "%m%y", pTime );
-  strcat( file,day);
-  strcat( file,"Paris");
-  strcat( file,mmaa);
-  strcat( file,".xml");  
-  strcat(repertory, file);
-  CURL *curl;
-  CURLcode res;
-  struct FtpFile ftpfile = {
-    file, /* name to store the file as if successful */ 
-    NULL
-  };
-  curl_global_init(CURL_GLOBAL_DEFAULT);
- 
-  curl = curl_easy_init();
-  if(curl) {
-    /*
-     * You better replace the URL with one that works!
-     */ 
-     printf("%s\n",repertory);
-    curl_easy_setopt(curl, CURLOPT_URL,
-                     repertory);                 
-    /* Define our callback to get called when there's data to be written */ 
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_fwrite);
-    /* Set a pointer to our struct to pass to the callback */ 
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ftpfile);
- 
-    /* Switch on full protocol/debug output */ 
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
- 
-    res = curl_easy_perform(curl);
- 
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
- 
-    if(CURLE_OK != res) {
-      /* we failed */ 
-      fprintf(stderr, "curl told us %d\n", res);
-    }
-  }
- 
-  if(ftpfile.stream)
-    fclose(ftpfile.stream); /* close the local file */ 
- 
-  curl_global_cleanup();
+  
+  int test = fusion();
  
   return 0;
 }
